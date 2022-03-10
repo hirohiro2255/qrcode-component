1403c203f59bd7ec900b88e297049b5bde802c66
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2022-03-10 14:20:16 +0900

    Create style.css

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,15 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, div, h1, img, p, section, text)
+import Html.Attributes exposing (alt, class, src)
 
 
 main : Html msg
 main =
-    text "Hi"
+    section [ class "qr-container" ]
+        [ div [ class "qr-img-container" ] [ img [ src "images/image-qr-code.png", class "qr-img", alt "qr-img" ] [] ]
+        , div [ class "qr-description-container" ]
+            [ h1 [] [ text "Improve your front-end skills by building projects" ]
+            , p [] [ text "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level" ]
+            ]
+        ]
